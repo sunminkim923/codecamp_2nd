f9112f9e7252cbecb3258384e79416d866581ec8
sunminkim923 <86465929+sunminkim923@users.noreply.github.com>
2021-08-16 23:44:06 +0900

    update

--- codecamp_project/src/components/units/market/commentwrite/commentwrite.container.tsx
+++ codecamp_project/src/components/units/market/commentwrite/commentwrite.container.tsx
@@ -1,19 +1,37 @@
 import { useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import CommentWriteUI from "./commentwrite.presenter";
 import { CREATE_USEDITEM_QUESTION } from "./commentwrite.queries";
 
 export default function CommentWrite() {
   const [createUseditemQuestion] = useMutation(CREATE_USEDITEM_QUESTION);
+  const router = useRouter();
+  const [contents, setContents] = useState("");
 
-  const onClickRegister = () => {
-    createUseditemQuestion({
-      variables: {
-        createUseditemQuestionInput: {
-          contents,
-        },
-      },
-    });
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
   };
 
-  return <CommentWriteUI onClickRegister={onClickRegister} />;
+  const onClickRegister = async (data) => {
+    try {
+      await createUseditemQuestion({
+        variables: {
+          createUseditemQuestionInput: contents,
+          useditemId: router.query.id,
+        },
+      });
+      alert("댓글을 등록합니다.");
+      console.log(data);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <CommentWriteUI
+      onClickRegister={onClickRegister}
+      onChangeContents={onChangeContents}
+    />
+  );
 }
