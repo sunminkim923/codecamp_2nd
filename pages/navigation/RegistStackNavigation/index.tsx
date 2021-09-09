51aecbe95982db6aabc30a435d7deedc770d1cf4
Bu-Du-Dak <guri930219@gmail.com>
2021-09-09 21:21:43 +0900

    Add RegistStackNavigation Index

---
+++ pages/navigation/RegistStackNavigation/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RegistStackNavigation from '../../../src/components/units/navigation/registStackNavigation/registStackNavigation.container';
+const RegistStackNavigationPage = () => {
+  return <RegistStackNavigation />;
+};
+export default RegistStackNavigationPage;
