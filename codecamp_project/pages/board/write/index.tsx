7d915f806db487aacbb3a85b4b532d93015053ed
sunminkim923 <tjsals0406@gmail.com>
2021-10-21 14:57:50 +0900

    update

--- codecamp_project/pages/board/write/index.tsx
+++ codecamp_project/pages/board/write/index.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line react/display-name
 import BoardWrite from "../../../src/components/units/board/write/boardWrite.container";
 
 export default function BoardWritePage() {
